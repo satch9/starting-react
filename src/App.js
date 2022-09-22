bbdf5eaf1cf99a303a10e76a6ec4ed9224b18c4e
satch9 <viny1976@gmail.com>
2022-09-22 11:47:54 +0400

    CSS in JS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
+import styled from '@emotion/styled'
 
 
 const PokemonRow = ({ pokemon, onSelect }) => (
@@ -23,7 +24,27 @@ PokemonRow.propTypes = {
   onSelect: PropTypes.func.isRequired
 }
 
+const Title = styled.h1`
+  text-align: center;
+`
 
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  column-gap: 1rem;
+`
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  paddingTop: 1rem;
+`
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`
 
 const PokemonInfo = ({ name, base }) => (
   <div>
@@ -67,31 +88,18 @@ function App() {
   //console.log(filter)
   //console.log(selectedItem)
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:3000/starting-react/pokemon.json')
-      .then(response=> response.json())
+      .then(response => response.json())
       .then(data => setPokemon(data))
-  },[])
+  }, [])
 
   return (
-    <div
-      style={{
-        margin: 'auto',
-        width: 800,
-        paddingTop: '1rem',
-      }}
-    >
-      <h1 className="title">Pokemon search</h1>
-
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '70% 30%',
-          columnGap: '1rem'
-        }}
-      >
+    <Container>
+      <Title>Pokemon search</Title>
+      <TwoColumnLayout>
         <div>
-          <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+          <Input value={filter} onChange={(e) => setFilter(e.target.value)} />
           <table width="100%">
             <thead>
               <tr>
@@ -118,9 +126,8 @@ function App() {
             <PokemonInfo {...selectedItem} />
           )
         }
-      </div>
-
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
